86430451d681e5dd27ee2c5420ef41c11c9ebae0
Mummadi Saikrishna <mummadisaikrishna4@gmail.com>
2026-01-06 15:44:30 +0530

    Sidebar Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,39 @@ import Login from "./pages/Login.jsx";
 import ProtectedRoute from "./routes/ProtectedRoute.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Employees from "./pages/Employees";
-function App() {
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import { AppSidebar } from "./components/ui/app-sidebar";
+function App({ children }) {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/employees"
-          element={
-            <ProtectedRoute>
-              <Employees />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="w-full">
+        <SidebarTrigger />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/employees"
+              element={
+                <ProtectedRoute>
+                  <Employees />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+        {children}
+      </main>
+    </SidebarProvider>
   );
 }
 
