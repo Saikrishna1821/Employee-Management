cf677dd51e532f22605479715310ddfcc43b4d91
Mummadi Saikrishna <mummadisaikrishna4@gmail.com>
2026-01-17 18:30:33 +0530

    default values for dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -33,25 +33,22 @@ export const Dashboard = () => {
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-6">Dashboard</h1>
       <div className="flex justify-center flex-col overflow-hidden carousel">
-        <div  className="h-42" >
+        <div className="h-42">
           <Carousel
-          plugins={[
-            Autoplay({
-              delay: 2000,
-            }),
-          ]}
-         
-          
-
+            plugins={[
+              Autoplay({
+                delay: 2000,
+              }),
+            ]}
           >
-            <CarouselContent >
-              <CarouselItem  className="flex justify-center">
-                <img src={teamwork1}  />
+            <CarouselContent>
+              <CarouselItem className="flex justify-center">
+                <img src={teamwork1} />
               </CarouselItem>
-              <CarouselItem  className="flex justify-center">
+              <CarouselItem className="flex justify-center">
                 <img src={teamwork2} />
               </CarouselItem>
-              <CarouselItem  className="flex justify-center">
+              <CarouselItem className="flex justify-center">
                 <img src={teamwork3} />
               </CarouselItem>
             </CarouselContent>
@@ -63,20 +60,20 @@ export const Dashboard = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <div className=" p-4 rounded-lg shadow">
             <p className="text-sm text-gray-500">Total Employees</p>
-            <p className="text-3xl font-bold">{totalEmployees}</p>
+            <p className="text-3xl font-bold">{totalEmployees ?? "0"}</p>
           </div>
 
           <div className=" p-4 rounded-lg shadow">
             <p className="text-sm text-gray-500">Active Employees</p>
             <p className="text-3xl font-bold text-green-600">
-              {activeEmployees}
+              {activeEmployees ?? "0"}
             </p>
           </div>
 
           <div className=" p-4 rounded-lg shadow">
             <p className="text-sm text-gray-500">Inactive Employees</p>
             <p className="text-3xl font-bold text-red-600">
-              {inactiveEmployees}
+              {inactiveEmployees ?? "0"}
             </p>
           </div>
         </div>
